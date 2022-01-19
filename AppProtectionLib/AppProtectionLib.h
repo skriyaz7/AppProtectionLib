a116919b5f215f8b37808b98aa49fb83fb772986
shaikr <shaik.riyaz@citrix.com>
2022-01-19 18:18:04 +0530

    initial project setup

---
+++ AppProtectionLib/AppProtectionLib.h
@@ -0,0 +1,18 @@
+//
+//  AppProtectionLib.h
+//  AppProtectionLib
+//
+//  Created by shaik riyaz on 19/01/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AppProtectionLib.
+FOUNDATION_EXPORT double AppProtectionLibVersionNumber;
+
+//! Project version string for AppProtectionLib.
+FOUNDATION_EXPORT const unsigned char AppProtectionLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppProtectionLib/PublicHeader.h>
+
+
---
+++ AppProtectionLibExamples/AppProtectionLibExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppProtectionLibExamplesApp.swift
+//  AppProtectionLibExamples
+//
+//  Created by shaik riyaz on 19/01/22.
+//
+
+import SwiftUI
+
+@main
+struct AppProtectionLibExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
